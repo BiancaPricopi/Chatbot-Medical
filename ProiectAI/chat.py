f7e7d49912e3faa0e793d252bbe37d18d4c30b7f
mandreea21 <mmariandreea01@gmail.com>
2023-01-24 16:07:21 +0200

    structural change

--- ProiectAI/chat.py
+++ ProiectAI/chat.py
@@ -3,15 +3,15 @@ import json
 
 import torch
 
-from model.model import NeuralNet
-from utils.nltk_utils import bag_of_words, tokenize
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('database/intents.json', 'r') as json_data:
+with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "database/data.pth"
+FILE = "data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
--- ProiectAI/train.py
+++ ProiectAI/train.py
@@ -5,10 +5,10 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-from utils.nltk_utils import bag_of_words, tokenize, stem
-from model.model import NeuralNet
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet
 
-with open('/database/intents.json', 'r') as f:
+with open('intents.json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
@@ -122,7 +122,7 @@ data = {
 "tags": tags
 }
 
-FILE = "../database/data.pth"
+FILE = "data.pth"
 torch.save(data, FILE)
 
 print(f'training complete. file saved to {FILE}')
